2b24616c6c365500fa2e9bbff91902edfc2b2c1c
Abriyan Yusuf <75099664+abriyanyusuf@users.noreply.github.com>
2023-05-28 09:58:44 +0700

    Update CSV_to_TFRecord_Converter.py
    Edit the code manually because after we change the extension .py the code is broken

--- CSV_to_TFRecord_Converter.py
+++ CSV_to_TFRecord_Converter.py
@@ -1,161 +1,121 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyMPEGdd8YtcOec1hjAKFF0F",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/abriyanyusuf/C23PS423_ML/blob/main/CSV_to_TFRecord_Converter.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "BzMMHJjATQY4"
-      },
-      "outputs": [],
-      "source": [
-        "##Convert CSV to TFRecord\n",
-        "# Script to create TFRecord files from train and test dataset folders\n",
-        "# Originally from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/generate_tfrecord.py\n",
-        "\n",
-        "\"\"\"\n",
-        "Usage:\n",
-        "  # From tensorflow/models/\n",
-        "  # Create train data:\n",
-        "  python generate_tfrecord.py --csv_input=images/train_labels.csv --image_dir=images/train --output_path=train.record\n",
-        "\n",
-        "  # Create test data:\n",
-        "  python generate_tfrecord.py --csv_input=images/test_labels.csv  --image_dir=images/test --output_path=test.record\n",
-        "\"\"\"\n",
-        "from __future__ import division\n",
-        "from __future__ import print_function\n",
-        "from __future__ import absolute_import\n",
-        "\n",
-        "import os\n",
-        "import io\n",
-        "import pandas as pd\n",
-        "\n",
-        "from tensorflow.python.framework.versions import VERSION\n",
-        "if VERSION >= \"2.0.0a0\":\n",
-        "    import tensorflow.compat.v1 as tf\n",
-        "else:\n",
-        "    import tensorflow as tf\n",
-        "\n",
-        "from PIL import Image\n",
-        "from object_detection.utils import dataset_util\n",
-        "from collections import namedtuple, OrderedDict\n",
-        "\n",
-        "flags = tf.app.flags\n",
-        "flags.DEFINE_string('csv_input', '', 'Path to the CSV input')\n",
-        "flags.DEFINE_string('labelmap', '', 'Path to the labelmap file')\n",
-        "flags.DEFINE_string('image_dir', '', 'Path to the image directory')\n",
-        "flags.DEFINE_string('output_path', '', 'Path to output TFRecord')\n",
-        "FLAGS = flags.FLAGS\n",
-        "\n",
-        "def split(df, group):\n",
-        "    data = namedtuple('data', ['filename', 'object'])\n",
-        "    gb = df.groupby(group)\n",
-        "    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]\n",
-        "\n",
-        "\n",
-        "def create_tf_example(group, path):\n",
-        "    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:\n",
-        "        encoded_jpg = fid.read()\n",
-        "    encoded_jpg_io = io.BytesIO(encoded_jpg)\n",
-        "    image = Image.open(encoded_jpg_io)\n",
-        "    width, height = image.size\n",
-        "\n",
-        "    filename = group.filename.encode('utf8')\n",
-        "    image_format = b'jpg'\n",
-        "    xmins = []\n",
-        "    xmaxs = []\n",
-        "    ymins = []\n",
-        "    ymaxs = []\n",
-        "    classes_text = []\n",
-        "    classes = []\n",
-        "\n",
-        "    labels = []\n",
-        "    with open(FLAGS.labelmap, 'r') as f:\n",
-        "        labels = [line.strip() for line in f.readlines()]\n",
-        "\n",
-        "    for index, row in group.object.iterrows():\n",
-        "        xmins.append(row['xmin'] / width)\n",
-        "        xmaxs.append(row['xmax'] / width)\n",
-        "        ymins.append(row['ymin'] / height)\n",
-        "        ymaxs.append(row['ymax'] / height)\n",
-        "        classes_text.append(row['class'].encode('utf8'))\n",
-        "        classes.append(int(labels.index(row['class'])+1))\n",
-        "\n",
-        "    tf_example = tf.train.Example(features=tf.train.Features(feature={\n",
-        "        'image/height': dataset_util.int64_feature(height),\n",
-        "        'image/width': dataset_util.int64_feature(width),\n",
-        "        'image/filename': dataset_util.bytes_feature(filename),\n",
-        "        'image/source_id': dataset_util.bytes_feature(filename),\n",
-        "        'image/encoded': dataset_util.bytes_feature(encoded_jpg),\n",
-        "        'image/format': dataset_util.bytes_feature(image_format),\n",
-        "        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),\n",
-        "        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),\n",
-        "        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),\n",
-        "        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),\n",
-        "        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),\n",
-        "        'image/object/class/label': dataset_util.int64_list_feature(classes),\n",
-        "    }))\n",
-        "    return tf_example\n",
-        "\n",
-        "\n",
-        "def main(_):\n",
-        "    # Load and prepare data\n",
-        "    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)\n",
-        "    path = os.path.join(os.getcwd(), FLAGS.image_dir)\n",
-        "    examples = pd.read_csv(FLAGS.csv_input)\n",
-        "\n",
-        "    # Create TFRecord files\n",
-        "    grouped = split(examples, 'filename')\n",
-        "    for group in grouped:\n",
-        "        tf_example = create_tf_example(group, path)\n",
-        "        writer.write(tf_example.SerializeToString())\n",
-        "\n",
-        "    writer.close()\n",
-        "    output_path = os.path.join(os.getcwd(), FLAGS.output_path)\n",
-        "    print('Successfully created the TFRecords: {}'.format(output_path))\n",
-        "\n",
-        "    # Create labelmap.pbtxt file\n",
-        "    path_to_labeltxt = os.path.join(os.getcwd(), FLAGS.labelmap)\n",
-        "    with open(path_to_labeltxt, 'r') as f:\n",
-        "        labels = [line.strip() for line in f.readlines()]\n",
-        "    \n",
-        "    path_to_labelpbtxt = os.path.join(os.getcwd(), 'labelmap.pbtxt')\n",
-        "    with open(path_to_labelpbtxt,'w') as f:\n",
-        "        for i, label in enumerate(labels):\n",
-        "            f.write('item {\\n' +\n",
-        "                    '  id: %d\\n' % (i + 1) +\n",
-        "                    '  name: \\'%s\\'\\n' % label +\n",
-        "                    '}\\n' +\n",
-        "                    '\\n')\n",
-        "\n",
-        "if __name__ == '__main__':\n",
-        "    tf.app.run()"
-      ]
-    }
-  ]
-}
+##Convert CSV to TFRecord
+# Script to create TFRecord files from train and test dataset folders
+# Originally from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/generate_tfrecord.py
+
+"""
+Usage:
+  # From tensorflow/models/
+  # Create train data:
+  python generate_tfrecord.py --csv_input=images/train_labels.csv --image_dir=images/train --output_path=train.record
+
+  # Create test data:
+  python generate_tfrecord.py --csv_input=images/test_labels.csv  --image_dir=images/test --output_path=test.record
+"""
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import io
+import pandas as pd
+
+from tensorflow.python.framework.versions import VERSION
+if VERSION >= "2.0.0a0":
+    import tensorflow.compat.v1 as tf
+else:
+    import tensorflow as tf
+
+from PIL import Image
+from object_detection.utils import dataset_util
+from collections import namedtuple, OrderedDict
+
+flags = tf.app.flags
+flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
+flags.DEFINE_string('labelmap', '', 'Path to the labelmap file')
+flags.DEFINE_string('image_dir', '', 'Path to the image directory')
+flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+FLAGS = flags.FLAGS
+
+def split(df, group):
+    data = namedtuple('data', ['filename', 'object'])
+    gb = df.groupby(group)
+    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+
+
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    classes_text = []
+    classes = []
+
+    labels = []
+    with open(FLAGS.labelmap, 'r') as f:
+        labels = [line.strip() for line in f.readlines()]
+
+    for index, row in group.object.iterrows():
+        xmins.append(row['xmin'] / width)
+        xmaxs.append(row['xmax'] / width)
+        ymins.append(row['ymin'] / height)
+        ymaxs.append(row['ymax'] / height)
+        classes_text.append(row['class'].encode('utf8'))
+        classes.append(int(labels.index(row['class'])+1))
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+def main(_):
+    # Load and prepare data
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    path = os.path.join(os.getcwd(), FLAGS.image_dir)
+    examples = pd.read_csv(FLAGS.csv_input)
+
+    # Create TFRecord files
+    grouped = split(examples, 'filename')
+    for group in grouped:
+        tf_example = create_tf_example(group, path)
+        writer.write(tf_example.SerializeToString())
+
+    writer.close()
+    output_path = os.path.join(os.getcwd(), FLAGS.output_path)
+    print('Successfully created the TFRecords: {}'.format(output_path))
+
+    # Create labelmap.pbtxt file
+    path_to_labeltxt = os.path.join(os.getcwd(), FLAGS.labelmap)
+    with open(path_to_labeltxt, 'r') as f:
+        labels = [line.strip() for line in f.readlines()]
+    
+    path_to_labelpbtxt = os.path.join(os.getcwd(), 'labelmap.pbtxt')
+    with open(path_to_labelpbtxt,'w') as f:
+        for i, label in enumerate(labels):
+            f.write('item {\n' +
+                    '  id: %d\n' % (i + 1) +
+                    '  name: \'%s\'\n' % label +
+                    '}\n' +
+                    '\n')
+
+if __name__ == '__main__':
+    tf.app.run()
